cbb4ecf9923971664f4eae6fbdab46d553eca4b9
Kasey Arnold <kaseyarnold15@gmail.com>
2020-01-27 15:47:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,8 +2,8 @@ def countdown(number) #just placed here
     while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
-    end
-return "HAPPY NEW YEAR!"
+end
+"HAPPY NEW YEAR!"
 
 def countdown_with_sleep(num)
   sleep (num)
