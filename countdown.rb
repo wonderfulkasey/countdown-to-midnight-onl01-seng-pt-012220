335f8910aed58b7f0d3c732f8b89b417ccde3ae6
Kasey Arnold <kaseyarnold15@gmail.com>
2020-01-27 15:44:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
-#write your code here
+def countdown #just placed here
 
-def countdown
+number = 10
+
+while number > 0
+puts "#{number} SECOND(S)!"
+number -= 1
+ 
+end
+
+return "HAPPY NEW YEAR!"
+
+#def countdown, originally here
 end
